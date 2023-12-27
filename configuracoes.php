4b4eb2567fb3080d561a7e2307fcc607d168c2dc
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2023-12-26 21:07:22 -0300

    Add files via upload

--- configuracoes.php
+++ configuracoes.php
@@ -1,5 +1,5 @@
 <?php
 # FileName="configuracoes.php"
 //define( 'CARACTERES', 'ISO-8859-1');	// para funcionar remotamente
-define( 'CARACTERES', 'UTF-8');	// para funcionar localmente
+//define( 'CARACTERES', 'UTF-8');	// para funcionar localmente
 ?>
