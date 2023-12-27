058eb3565e76d38d7080240c95f37479263a5d3c
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2023-12-27 18:59:35 -0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,5 @@
 <?php
 # FileName="connect.php"
-/*	$hostname = "sql206.epizy.com";
-	$database = "epiz_33999511_academia";
-	$username = "epiz_33999511";
-	$password = "lns2018WvpgLI"; */
 $hostname = "localhost";
 $database = "ficha";
 $jacknpoe = "jacknpoe";
